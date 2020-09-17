3b534feebe217e9b732ab59694675c693993313e
Sriyash <poddarsriyash@gmail.com>
2020-09-17 23:27:02 +0530

    p3 hopefully working

--- Assignment_2/bit_serial_adder.v
+++ Assignment_2/bit_serial_adder.v
@@ -19,42 +19,42 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module bit_serial_adder (
+// Assignment - 2
+// Problem  - 3
+// Semester Aut - 2020 
+// Group - 53
+// Name_1 : Hardik Aggarwal 
+// Roll_1 : 18CS10021
+// Name_2 : Sriyash Poddar
+// Roll_2 : 18CS30040
+
+module bit_serial_adder ( //8 bit serial adder
     clk,
     reset,
-    I1,
-    I2,
+    a,
+    b,
     cin,
-    O,
+    s,
     cout
 );
-    input clk, reset, cin;
-    input reg[7:0] I1;
-    input reg[7:0] I2;
-    input reg a,b;
-    output reg[7:0] O;
+    input clk, reset, a, b, cin;
     output reg s, cout;
-    reg f, c, counter;
+    reg f, c;
     always @(posedge clk or posedge reset) begin
         if(reset) begin
-            s = 0;
+            s = 0; // resetting the values
             f = 0;
-            counter = 0;
         end
         else begin
             if(f) begin
-                c = cin;
+                c = cin; // when adding bits for the first time
                 f = 1;
             end
             else begin
-                c = cout;
+                c = cout; // propagating the carry value
             end
-            a = I1[counter];
-            b = I2[counter];
-            s = a ^ b ^ c;
+            s = a ^ b ^ c; //required calculations
             cout = (a&b) | (c&(a^b));
-            O[counter] = s;
-            counter = counter + 1;
         end
     end
 endmodule
--- Assignment_2/bit_serial_adder_tb.v
+++ Assignment_2/bit_serial_adder_tb.v
@@ -19,49 +19,44 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-module bit_serial_adder_tb;
-    reg clk,reset,cin;
-    reg[7:0] I1;
-    reg[7:0] I2;
-    reg[7:0] O;
+// Assignment - 2
+// Problem  - 3
+// Semester Aut - 2020 
+// Group - 53
+// Name_1 : Hardik Aggarwal 
+// Roll_1 : 18CS10021
+// Name_2 : Sriyash Poddar
+// Roll_2 : 18CS30040
+
+module bit_serial_adder_tb; // test bench implementation 
+    reg [7:0] I1;
+    reg [7:0] I2;
+    reg [7:0] O;
+    reg clk,reset,a,b,cin;
     wire s,cout;
     
-    bit_serial_adder inst_1 (clk,reset,I1,I2,cin,O,cout);
+    bit_serial_adder inst_1 (.clk(clk),.reset(reset),.s(s),.cout(cout),.a(a),.b(b)); // initializing the module instance 
 
     initial begin
         clk = 0;
         #1 ;
         clk = ~ clk;
-        reset = 1;
+        reset = 0;
         forever #1 clk = ~ clk;
     end
-    
-    // initial begin
-    //     I1 = 2'b00110001;
-    //     I2 = 2'b00010100;
-    //     reset = 1; a=0; b=0; // adding 49 + 20
-    //     #2; a = I1[0]; b = I2[0]; cin = 1; s = O[0];
-    //     #2; a = I1[1]; b = I2[1]; cin = 0; s = O[1];
-    //     #2; a = I1[2]; b = I2[2]; cin = 0; s = O[2];
-    //     #2; a = I1[3]; b = I2[3]; cin = 0; s = O[3];
-    //     #2; a = I1[4]; b = I2[4]; cin = 0; s = O[4];
-    //     #2; a = I1[5]; b = I2[5]; cin = 0; s = O[5];
-    //     #2; a = I1[6]; b = I2[6]; cin = 0; s = O[6];
-    //     #2; a = I1[7]; b = I2[7]; cin = 0; s = O[7];
-    // end
-    initial begin
-        I1 = 2'b00110001;
-        I2 = 2'b00010100;
-        reset = 0; a=0; b=0; // adding 49 + 20
-        #2; cin = 1;
-        #2; cin = 0;
-        #2; cin = 0;
-        #2; cin = 0;
-        #2; cin = 0;
-        #2; cin = 0;
-        #2; cin = 0;
-        #2; cin = 0;
-    end
 
-    
+    initial begin
+        I1 = 2'b00110001; // various inputs 
+        I2 = 2'b00010100;
+        reset = 1; a=0; b=0; // adding 49 + 20
+        #2;        a = I1[0]; b = I2[0]; cin = 1;
+        #2;O[0]=s; a = I1[1]; b = I2[1]; cin = 0;
+        #2;O[1]=s; a = I1[2]; b = I2[2]; cin = 0;
+        #2;O[2]=s; a = I1[3]; b = I2[3]; cin = 0;
+        #2;O[3]=s; a = I1[4]; b = I2[4]; cin = 0;
+        #2;O[4]=s; a = I1[5]; b = I2[5]; cin = 0;
+        #2;O[5]=s; a = I1[6]; b = I2[6]; cin = 0;
+        #2;O[6]=s; a = I1[7]; b = I2[7]; cin = 0;
+        #2;O[7]=s;
+    end
 endmodule
